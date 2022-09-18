d19f9c7bffd6c7927aab77a84cb3ffee474a966f
Admin <preciouspeter3173@gmail.com>
2022-09-18 17:55:30 +0100

    sub settings added

--- app/src/main/java/com/example/mfpa/SettingsFragment.kt
+++ app/src/main/java/com/example/mfpa/SettingsFragment.kt
@@ -1,11 +1,24 @@
 package com.example.mfpa
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import java.util.prefs.Preferences
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+       val accSettings = findPreference<Preference>("key_Settings")
+
+        accSettings!!.setOnPreferenceClickListener{
+
+        findNavController().navigate(R.id.action_settingsFragment_to_privacySetting)
+
+            true
+        }
+
     }
 }
