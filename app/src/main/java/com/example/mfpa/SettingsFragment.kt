f62a249afe8ecc45ec7bc5d57ebcfe9cf0fb00ed
Admin <preciouspeter3173@gmail.com>
2022-09-19 14:13:12 +0100

    drawerLayout added

--- app/src/main/java/com/example/mfpa/SettingsFragment.kt
+++ app/src/main/java/com/example/mfpa/SettingsFragment.kt
@@ -1,15 +1,22 @@
 package com.example.mfpa
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.preference.Preference
+import androidx.preference.PreferenceDataStore
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import java.util.prefs.Preferences
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val dataStore = DataStores()
 
        val accSettings = findPreference<Preference>("key_Settings")
 
@@ -18,6 +25,35 @@ class SettingsFragment : PreferenceFragmentCompat() {
         findNavController().navigate(R.id.action_settingsFragment_to_privacySetting)
 
             true
+        }
+
+
+        val notification = findPreference<SwitchPreference>("key_notification")
+
+        notification?.summaryProvider = Preference.SummaryProvider<SwitchPreference> {
+            switchNot ->
+
+            if (switchNot?.isChecked!!)
+                "Status: ON"
+            else "Status: OFF"
+
+        }
+
+        notification?.preferenceDataStore = dataStore
+    }
+
+    class DataStores: PreferenceDataStore(){
+        override fun getBoolean(key: String?, defValue: Boolean): Boolean {
+
+            return  defValue
+        }
+
+
+        override fun putBoolean(key: String?, value: Boolean) {
+            if (key == "key_notification"){
+
+                
+            }
         }
 
     }
