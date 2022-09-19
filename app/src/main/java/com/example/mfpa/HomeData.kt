dd9fcda4e55957e347f79ca183079f1e8f4997b6
Admin <preciouspeter3173@gmail.com>
2022-09-19 19:11:09 +0100

    data class added

---
+++ app/src/main/java/com/example/mfpa/HomeData.kt
@@ -0,0 +1,5 @@
+package com.example.mfpa
+
+data class HomeData (
+    var image: Int, var text: String, var isFavourite: Boolean
+        )
--- app/src/main/java/com/example/mfpa/MainActivity.kt
+++ app/src/main/java/com/example/mfpa/MainActivity.kt
@@ -3,17 +3,28 @@ package com.example.mfpa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.mfpa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val  view = binding.root
+        setContentView(view)
 
         val navHostFragment= supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment?
 
         val navController = navHostFragment!!.navController
 
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
 
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+      binding.navView.setupWithNavController(navController)
     }
 
 }
